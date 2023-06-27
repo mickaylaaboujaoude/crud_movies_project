00873bc756e0613aaf9394cae11ef4fdcef00dc8
mickaylaaboujaoude <88628010+mickaylaaboujaoude@users.noreply.github.com>
2023-06-26 20:20:44 -0400

    Testing SQL connection

--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const app = express()
 const mysql = require('msql')
+const config = require('./config');
+
 
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: ''
-    database: 'database'
+    password: config[ 'db' ][ 'password' ],
+    database: config[ 'db' ][ 'database' ]
 })
 
+
 app.get('/', (req, res)=> {
-    res.send('hello world dude');
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('Shrek', 'Good movie')"
+    db.query(sqlInsert, (error, result)=> {
+        res.send('Inserted');
+    })
+    
 })
 
 app.listen(3001, () => {
